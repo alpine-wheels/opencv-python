7d175075ce8b4ed822ff39e2bc58ecaf5481282b
William Jackson <william@subtlecoolness.com>
2020-07-15 11:23:38 -0500

    python-opencv==3.4.10.35

--- build.sh
+++ build.sh
@@ -2,8 +2,10 @@
 
 set -e
 
-# install os dependencies
-# apk add --no-cache gcc musl-dev
+# install dependencies
+apk add --no-cache g++ git make
+apk add --no-cache --repository http://dl-cdn.alpinelinux.org/alpine/v3.9/main/ qt-dev
+pip install --extra-index-url https://alpine-wheels.github.io/index cmake==3.17.3 numpy==1.17.3
 
 # build
-pip wheel --no-deps --requirement requirements.txt
+pip wheel --no-deps git+https://github.com/skvark/opencv-python@35

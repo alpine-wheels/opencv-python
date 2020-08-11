2a04a37d5ee8553572990fe4d421a02c2bb0d04d
William Jackson <william@subtlecoolness.com>
2020-08-11 09:05:22 -0500

    Update build.sh

--- build.sh
+++ build.sh
@@ -8,4 +8,4 @@ apk add --no-cache --repository http://dl-cdn.alpinelinux.org/alpine/v3.9/main/ 
 pip install --extra-index-url https://alpine-wheels.github.io/index cmake==3.17.3 numpy==1.17.3
 
 # build
-pip wheel --no-deps git+https://github.com/skvark/opencv-python@36
+pip wheel --no-deps git+https://github.com/skvark/opencv-python@37
